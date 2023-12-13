ffa53f628e15508820859d6aa9d93ce991a8a281
Kelvin <kevohmasambu@gmail.com>
2023-12-14 00:58:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    const innerFunction = function() {
+        return `This Saturday, I want to ${activity}!`
+    }
+    return innerFunction()
+}
+console.log(saturdayFun("ball"))
+
+
+
+
+function mondayWork(activity = "go to the office") {
+    const innerFunction = function() {
+        return `This Monday, I will ${activity}.`
+    }
+    return innerFunction()    
+}
+console.log(mondayWork("watch a game"))
+
+
+
+
+function wrapAdjective(string = "*") {
+    return function(adjective = "special") {
+        return `You are ${string}${adjective}${string}!`
+    }
+}
+const encouragingPromptFunction = wrapAdjective("||")
+console.log(encouragingPromptFunction())
